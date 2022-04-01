2cb25615b7397b820763830667b0d1579be00f11
MamedovVasif <24Q8KZ1@code.edu.az>
2022-04-01 04:36:00 +0400

    PWR commit

---
+++ PowREXT/ConsoleApp1/Models/PowerExtension.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Models
+{
+    public static class PowerExtension
+    {
+        public static int PowerEXT(this int num1)
+        {
+            int result = 1;
+            for (int i = 2; i <=num1; i++)
+            {
+              result=  num1 * num1;
+            }
+            return result;
+        }
+    }
+}
---
+++ PowREXT/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using ConsoleApp1.Models;
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            Console.WriteLine(a.PowerEXT());
+        }
+    }
+}
